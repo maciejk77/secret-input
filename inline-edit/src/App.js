c2be467f2cf14ffeba481e39564aaa521e7bf068
Maciej Kurek <maciejk77@gmail.com>
2021-11-01 12:50:00 +0000

    First app completed

--- inline-edit/src/App.js
+++ inline-edit/src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Server } from 'miragejs';
 
-function App() {
+let server = new Server({ timing: 1000 });
+server.get('/api/input', {});
+server.post('/api/input', (_, request) => {
+  return JSON.parse(request.requestBody);
+});
+
+const App = () => {
+  const [loading, setIsLoading] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+  const [status, setStatus] = useState(null);
+
+  const fetchData = async () => {
+    await fetch('/api/input').then((json) => {
+      // console.log('json: ', JSON.parse(json._bodyInit));
+      const data = JSON.parse(json._bodyInit);
+      setInputValue(data.text);
+    });
+  };
+
+  useEffect(() => {
+    loading && fetchData();
+  }, []);
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setInputValue(value);
+  };
+
+  const handleBlur = async (e) => {
+    const { value } = e.target;
+    setIsLoading(true);
+    await fetch('/api/input', {
+      method: 'POST',
+      body: JSON.stringify({ input: value }),
+    });
+    setStatus('success');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={{ display: 'flex' }}>
+        <input value={inputValue} onChange={handleChange} onBlur={handleBlur} />
+        <div>{loading && <> loading... </>}</div>
+      </div>
+      <div>{status === 'success' && <>tick</>}</div>
+    </>
   );
-}
+};
 
 export default App;
